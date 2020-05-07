3cf0b516c70a968ccfdf0b57223e286e471faea7
Sergei Muravev <muraviev.sergey@gmail.com>
2020-05-07 22:44:46 +1000

    Added stack tests.

---
+++ stack/example_test.go
@@ -0,0 +1,50 @@
+package stack_test
+
+import (
+	"../linkedlist"
+	"../stack"
+	"fmt"
+)
+
+func Example() {
+	stack := stack.New(1, 2, 3)
+
+	// Push
+	stack.Push(4)
+	stack.Push(5)
+
+	// Count
+	count := stack.Count()
+	fmt.Println(count)
+
+	// Peek
+	next := stack.Peek()
+	fmt.Println(next)
+
+	// Enumerate
+	it := stack.GetEnumerator()
+	for it.MoveNext() {
+		node := it.Current().(*linkedlist.Node)
+		fmt.Println(node.Value())
+	}
+
+	// Pop
+	for stack.Count() > 0 {
+		next = stack.Pop()
+		fmt.Println(next)
+	}
+
+	// Output:
+	// 5
+	// 5
+	// 5
+	// 4
+	// 3
+	// 2
+	// 1
+	// 5
+	// 4
+	// 3
+	// 2
+	// 1
+}
